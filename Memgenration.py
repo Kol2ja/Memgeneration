6f4ddcc83f1ec7ac7df18b94919db848e7212eb6
User <Kol2ja@yandex.ru>
2018-12-24 16:19:06 +0300

    Закоментил неработающий код

--- Memgenration.py
+++ Memgenration.py
@@ -2,10 +2,14 @@ import sys
 from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
                              QAction, QFileDialog, QApplication)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QWidget, QPushButton
 from PyQt5.QtWidgets import QLabel, QLCDNumber, QLineEdit
-from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton,
-                             QSizePolicy, QLabel, QFontDialog, QApplication)
+from PyQt5.QtWidgets import QVBoxLayout, QSizePolicy, QFontDialog
+from PyQt5.QtWidgets import qApp
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
+                             QLabel, QApplication)
+from PyQt5.QtGui import QPixmap
 
 
 class Example(QWidget):
@@ -15,61 +19,61 @@ class Example(QWidget):
 
     def initUI(self):
         self.setGeometry(1, 30, 1260, 720)
-        self.setWindowTitle('Генератор мемов')
+        self.setWindowTitle('Ãåíåðàòîð ìåìîâ')
 
-        self.btn = QPushButton('Загрузить', self)
+        self.btn = QPushButton('Çàãðóçèòü', self)
         self.btn.resize(self.btn.sizeHint())
         self.btn.move(500, 100)
         self.btn.clicked.connect(self.hello)
 
-        self.btn = QPushButton('Сделать мем', self)
+        self.btn = QPushButton('Ñäåëàòü ìåì', self)
         self.btn.resize(self.btn.sizeHint())
         self.btn.move(500, 400)
         self.btn.clicked.connect(self.hello)
 
         self.label = QLabel(self)
-        self.label.setText(' ' * 140)
+        self.label.setText(' ')
         self.label.move(500, 200)
 
-        self.name_inpu3 = QPushButton('Скачать мем', self)
+        self.name_inpu3 = QPushButton('Ñêà÷àòü ìåì', self)
         self.btn.resize(self.btn.sizeHint())
-        self.name_inpu3.move(500, 500)
+        self.name_inpu3.move(500, 400)
 
-        self.name_inpu3 = QPushButton('Положение текста', self)
+        self.name_inpu3 = QPushButton('Ïîëîæåíèå òåêñòà', self)
         self.btn.resize(self.btn.sizeHint())
         self.name_inpu3.move(500, 300)
 
-        vbox = QVBoxLayout()
-
-        btn = QPushButton('Выбрать ширифт', self)
+        btn = QPushButton('Âûáðàòü øèðèôò', self)
         btn.setSizePolicy(QSizePolicy.Fixed,
                           QSizePolicy.Fixed)
 
-        btn.move(500, 200)
-
-        vbox.addWidget(btn)
+        btn.move(0, 0)
 
         btn.clicked.connect(self.showDialog)
 
-        self.lbl = QLabel('Knowledge only matters', self)
-        self.lbl.move(130, 20)
+        self.lbl = QLabel('Ïðèìåð òåêñòà', self)
 
-        vbox.addWidget(self.lbl)
-        self.setLayout(vbox)
+        self.lbl.move(0, 600)
 
-        #     self.textEdit = QTextEdit()
-        #    self.setCentralWidget(self.textEdit)
-        #   self.statusBar()
+        pixmap = QPixmap("13028.jpg")
 
-    #
-    #       openFile = QAction(QIcon('open.png'), 'Open', self)
-    #      openFile.setShortcut('Ctrl+O')
-    #     openFile.setStatusTip('Open new File')
-    #    openFile.triggered.connect(self.showDialogfile)
-    #
-    #       menubar = self.menuBar()
-    #      fileMenu = menubar.addMenu('&File')
-    #     fileMenu.addAction(openFile)
+        lbl = QLabel(self)
+        lbl.resize(500, 500)
+        lbl.move(0, 30)
+        lbl.setPixmap(pixmap)
+
+
+
+        # openFile = QAction(QIcon('open.png'), 'Open', self)
+        # openFile.setShortcut('Ctrl+O')
+        # openFile.setStatusTip('Open new File')
+        # openFile.triggered.connect(self.showDialogfile)
+
+        # self.statusBar()
+
+        # menubar = self.menuBar()
+        # fileMenu = menubar.addMenu('&File')
+        # fileMenu.addAction(openFile)
 
     def hello(self):
         pass
@@ -78,6 +82,8 @@ class Example(QWidget):
         font, ok = QFontDialog.getFont()
         if ok:
             self.lbl.setFont(font)
+            self.lbl.adjustSize()
+            print(font)
 
     def showDialogfile(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
@@ -87,6 +93,7 @@ class Example(QWidget):
         with f:
             data = f.read()
             self.textEdit.setText(data)
+            print(data)
 
 
 if __name__ == '__main__':
